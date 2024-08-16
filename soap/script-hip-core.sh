698c5aac9475068864ea1d85f2f7f7cb4985ac8c
Tiago Marques <tiago.woa@gmail.com>
2024-08-16 12:39:47 +0100

    refactor: updated files path

--- soap/script-hip-core.sh
+++
@@ -1,5 +0,0 @@
-kamel reset
-kubectl apply -f soap-to-rabbitmq-pipe.yaml
-kubectl apply -f soap-process-action.kamelet.yaml
-kamel delete soap-response-pipe
-kubectl apply -f soap-response-pipe.yaml
